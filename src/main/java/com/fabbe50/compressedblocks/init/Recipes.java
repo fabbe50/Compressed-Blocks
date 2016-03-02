cfa29e88af26054305cf94c4f30132d08a67feeb
William <fabbe50@gmail.com>
2016-03-02 16:36:09 +0100

    commit e4

--- src/main/java/com/fabbe50/compressedblocks/init/Recipes.java
+++ src/main/java/com/fabbe50/compressedblocks/init/Recipes.java
@@ -23,11 +23,18 @@ public class Recipes {
         //Recipe Markers
         String r = "sss";
 
-        //ItemStacks
+        //ItemStacks Vanilla
         ItemStack potato = new ItemStack(Items.potato);
+        ItemStack bone = new ItemStack(Items.bone);
+        ItemStack bakedpotato = new ItemStack(Items.baked_potato);
+
+        //ItemStacks Compressed Blocks
         ItemStack endgamium = new ItemStack(ModItems.endgamium);
         ItemStack endgamiumb = new ItemStack(ModBlocks.endgamiumblock);
         ItemStack endgamiumbc = new ItemStack(ModBlocks.endgamiumblockc);
+        ItemStack infusedbone = new ItemStack(ModItems.infusedbone);
+        ItemStack potatobone = new ItemStack(ModItems.potatobone);
+        ItemStack endgamiumnugget = new ItemStack(ModItems.endgamiumnugget);
 
         //ItemStack Array Apply
         for (int i = 0; i < potatoes.length; i++) {
@@ -36,10 +43,16 @@ public class Recipes {
 
         LogHelper.info("Adding Mod Recipes");
         //Standard Shapeless Recipes
+        GameRegistry.addShapelessRecipe(infusedbone, bone, endgamiumnugget);
+        GameRegistry.addShapelessRecipe(potatobone, bakedpotato, infusedbone);
+        GameRegistry.addShapelessRecipe(new ItemStack(ModBlocks.endgamiumblock, 9), endgamiumbc);
+        GameRegistry.addShapelessRecipe(new ItemStack(ModItems.endgamium, 9), endgamiumb);
+        GameRegistry.addShapelessRecipe(new ItemStack(ModItems.endgamiumnugget, 9), endgamium);
 
         //Standard Shaped Recipes
 
         //Full Block Recipes
+        GameRegistry.addShapedRecipe(endgamium, r, r, r, 's', endgamiumnugget);
         GameRegistry.addShapedRecipe(endgamiumb, r, r, r, 's', endgamium);
         GameRegistry.addShapedRecipe(endgamiumbc, r, r, r, 's', endgamiumb);
 
