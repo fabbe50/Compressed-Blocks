f800522f31f4082fad2a40b02b6fb02c7184c09f
William <fabbe50@gmail.com>
2016-04-29 10:30:01 +0200

    Starting Empty

--- src/api/java/seia/easymultiblock/api/package-info.java
+++
@@ -1,6 +0,0 @@
-/**
- * Easy Multiblock API. Made by SeiA.
- */
-@API(apiVersion="1.0", owner="SeiA", provides="EasyMultiblockAPI")
-package seia.easymultiblock.API;
-import cpw.mods.fml.common.API;
--- src/main/java/com/fabbe50/compressedblocks/core/proxy/IProxy.java
+++
@@ -1,7 +0,0 @@
-package com.fabbe50.compressedblocks.core.proxy;
-
-/**
- * Created by fabbe50 on 14/01/2016.
- */
-public interface IProxy {
-}
