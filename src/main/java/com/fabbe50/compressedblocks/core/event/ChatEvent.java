cbc6d577c2072896e6c3d744bd0f2dc577fc28fb
William <fabbe50@gmail.com>
2017-11-26 01:44:00 +0100

    Stub of chatbot

--- src/main/java/com/fabbe50/compressedblocks/core/event/ChatEvent.java
+++ src/main/java/com/fabbe50/compressedblocks/core/event/ChatEvent.java
@@ -1,9 +1,13 @@
 package com.fabbe50.compressedblocks.core.event;
 
+import com.michaelwflaherty.cleverbotapi.CleverBotQuery;
+import com.thefifthidiot.tficore.utility.LogHelper;
 import com.thefifthidiot.tficore.utility.helper.ChatHelper;
 import net.minecraft.util.text.TextComponentString;
 import net.minecraftforge.event.ServerChatEvent;
 import net.minecraftforge.fml.common.eventhandler.SubscribeEvent;
+
+import java.io.IOException;
 
 /**
  * Created by fabbe on 01/07/2017.
@@ -35,14 +39,26 @@ public class ChatEvent {
             String message = event.getMessage();
             if (message.toLowerCase().contains("!wake")) {
                 botActive = true;
+                event.getPlayer().sendMessage(new TextComponentString("<" + name + "> " + message));
                 event.getPlayer().sendMessage(new TextComponentString("[Compressed Bot] " + "I'm Awake!"));
+                event.setCanceled(true);
             }
             else if (message.toLowerCase().contains("!die")) {
                 botActive = false;
+                event.getPlayer().sendMessage(new TextComponentString("<" + name + "> " + message));
                 event.getPlayer().sendMessage(new TextComponentString("[Compressed Bot] " + "Committing Suicide... :("));
+                event.setCanceled(true);
             }
-            else {
-                //Bot-code goes here...
+            else if (botActive) {
+                event.getPlayer().sendMessage(new TextComponentString("<" + name + "> " + message));
+                /*CleverBotQuery query = new CleverBotQuery("", message);
+                try {
+                    query.sendRequest();
+                    event.getPlayer().sendMessage(new TextComponentString("[Compressed Bot] " + query.getResponse()));
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }*/
+                event.setCanceled(true);
             }
         }
     }
